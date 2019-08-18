0f57dd65b0d97e9e78a9bef1624d8b337e001779
pira <pira.y1121@gmail.com>
2019-08-19 00:11:06 +0900

    fix:TLEを修正した. close #3

--- ABC138/d.py
+++ ABC138/d.py
@@ -1,23 +1,18 @@
 n,q = map(int, input().split())
 
-tree = [[[] for i in range(2)] for j in range(n)]
+parentNodes = [0] * n #i番目のノードの親番号
 
 for i in range(n-1):
 	a,b = map(int, input().split())
-	tree[a-1][1].append(b-1)
-	tree[b-1][0].append(a-1)
+	parentNodes[b-1] = a-1
 
 counter = [0] * n
 
-for i in range(q):
+for _ in range(q):
 	p,x = map(int, input().split())
 	counter[p-1] += x
-	if tree[p-1][1] != []:
-		queue = tree[p-1][1].copy()
-		while queue != []:
-			node = queue[0]
-			counter[node] += x
-			queue.extend(tree[node][1])
-			queue.remove(node)
+
+for i in range(1,n):
+	counter[i] += counter[parentNodes[i]]
 
 print(*counter)
