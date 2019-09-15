981c32a51a9a220dff66e35d768c7c6d287e86d3
pira <pira.y1121@gmail.com>
2019-09-15 22:55:04 +0900

    fix:ABC141-dで毎回ソートや最大値のインデックスの探索をしているとTLEしてしまうため必要ないときはソートをしない実装に修正した．fix #4

--- ABC141/d.py
+++ ABC141/d.py
@@ -1,10 +1,20 @@
-import numpy as np
-
 N,M = map(int, input().split())
-A = np.array(list(map(int, input().split())))
+
+A = list(map(int, input().split()))
+A.sort(reverse=True)
+
+it = 0
 
 for _ in range(M):
-	index = A.argmax()
-	A[index] = A[index]//2
-	
+	A[it] = A[it]//2
+	if it < N-1:
+		if A[it] < A[it+1] and A[0] < A[it+1]:
+			it += 1	
+		else:
+			A.sort(reverse=True)
+			it = 0
+	elif it == N-1:
+		A.sort(reverse=True)
+		it = 0
+
 print(sum(A))
